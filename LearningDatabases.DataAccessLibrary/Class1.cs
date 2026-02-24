6084009ef681e66f729a9e79074bdca4c8087aca
Jakob Farian Krarup <kr@rup.dk>
2026-02-24 11:44:12 +0100

    Added comments and "using"

--- LearningDatabases.DataAccessLibrary/Class1.cs
+++ LearningDatabases.DataAccessLibrary/Class1.cs
@@ -14,28 +14,37 @@ public class AccountDataAccess
 
     public IEnumerable<Account> GetAllAccounts()
     {
-        //Instantiate a connection object which points to the correct server and database, based on the connection string
-        IDbConnection connection = new SqlConnection(ConnectionString);
-        //create a command object, which has a reference to the connection object and the SQL to perform
-        IDbCommand command = new SqlCommand("select * from account", (SqlConnection)connection);
-
-        //get a datareader, with acccess to the rows in the table which match the SQL query
-        var datareader = command.ExecuteReader();
-        //create an empty list to hold the Account objects
-        List<Account> accounts = new();
-        //as long as there are more tuples (based on our SQL), continue this loop
-        while (datareader.Read())
+        //NOTE: we're declaring and instantiating variables using the "using" keyword
+        //which closes and releases memory 
+        try
         {
-            //create an account object
-            var account = new Account()
+            //Instantiate a connection object which points to the correct server and database, based on the connection string
+           using IDbConnection connection = new SqlConnection(ConnectionString);
+            //create a command object, which has a reference to the connection object and the SQL to perform
+            IDbCommand command = new SqlCommand("select * from account", (SqlConnection)connection);
+            connection.Open();
+            //get a datareader, with acccess to the rows in the table which match the SQL query
+             var datareader = command.ExecuteReader();
+            //create an empty list to hold the Account objects
+            List<Account> accounts = new();
+            //as long as there are more tuples (based on our SQL), continue this loop
+            while (datareader.Read())
             {
-                Id = (int)datareader["Id"],
-                Name = (string)datareader["Name"],
-                Balance = (decimal)datareader["Balance"]
-            };
-            accounts.Add(account); //add it to the list
+                //create an account object
+                var account = new Account()
+                {
+                    Id = (int)datareader["Id"],
+                    Name = (string)datareader["Name"],
+                    Balance = (decimal)datareader["Balance"]
+                };
+                accounts.Add(account); //add it to the list
+            }
+            return accounts; //return the list
         }
-        return accounts; //return the list
+        catch (Exception ex)
+        {
+            throw new Exception("Error retrieving all Accounts", ex);
+        }
     }
 
 }
